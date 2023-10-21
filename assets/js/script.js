3393d5a6e3408edea071dcf50b21da487690d42d
Toni01~ <neonver.2020@gmail.com>
2023-10-21 16:16:24 +0900

    :Wq

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
 const buttonPasting = document.querySelector("#buttonPasting");
 const buttonDetailing = document.querySelector("#buttonDetailing");
+const images = document.querySelectorAll('.slider .slider-line .car__block');
+// const sliderLine = document.querySelector('.slider-line');
+// let count = 0;
+// let width;
 
 buttonPasting.addEventListener('click', (button) => {
-    console.log("sosi");
     buttonPasting.classList.add("active");
     buttonDetailing.classList.remove("active")
 });
+
 buttonDetailing.addEventListener('click', (button) => {
-    console.log("sosi");
     buttonDetailing.classList.add("active");
     buttonPasting.classList.remove("active")
 });
+
+// function init() {
+//     console.log('resize');
+//     width = document.querySelector('.slider').offsetWidth;
+//     sliderLine.style.width = width * images.length + 'px';
+//     images.forEach(item => {
+//         item.style.width = width + 'px';
+//     });
+// }
+
+// init();
