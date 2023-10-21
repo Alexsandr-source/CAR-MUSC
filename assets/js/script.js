2878d848f8799b4ff8c5006874c3926accdeb3b9
Toni01~ <neonver.2020@gmail.com>
2023-10-21 16:41:42 +0900

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 const buttonPasting = document.querySelector("#buttonPasting");
 const buttonDetailing = document.querySelector("#buttonDetailing");
 const images = document.querySelectorAll('.slider .slider-line .car__block');
-// const sliderLine = document.querySelector('.slider-line');
-// let count = 0;
-// let width;
 
 buttonPasting.addEventListener('click', (button) => {
     buttonPasting.classList.add("active");
@@ -14,14 +11,3 @@ buttonDetailing.addEventListener('click', (button) => {
     buttonDetailing.classList.add("active");
     buttonPasting.classList.remove("active")
 });
-
-// function init() {
-//     console.log('resize');
-//     width = document.querySelector('.slider').offsetWidth;
-//     sliderLine.style.width = width * images.length + 'px';
-//     images.forEach(item => {
-//         item.style.width = width + 'px';
-//     });
-// }
-
-// init();
