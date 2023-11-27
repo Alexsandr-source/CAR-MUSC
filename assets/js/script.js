0194c9c2164b8c52975ce0876d442e4ea0ef7f53
Toni01~ <neonver.2020@gmail.com>
2023-11-27 20:53:53 +0900

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,36 @@
 const buttonPasting = document.querySelector('#buttonPasting');
 const buttonDetailing = document.querySelector('#buttonDetailing');
-const slider = document.getElementsByClassName('slider-line');
 const slides = document.getElementsByClassName('car__section');
-const prevButton = document.getElementsByClassName('slide-prev');
-const nextButton = document.getElementsByClassName('slide-next');
+// const prevButton = document.getElementsByClassName('slide-prev');
+// const nextButton = document.getElementsByClassName('slide-next');
 
-buttonPasting.addEventListener('click', (button) => {
+// OUR SERVICES button //
+
+buttonPasting.addEventListener('click', (_button) => {
     buttonPasting.classList.add("active");
     buttonDetailing.classList.remove("active")
 });
 
-buttonDetailing.addEventListener('click', (button) => {
+buttonDetailing.addEventListener('click', (_button) => {
     buttonDetailing.classList.add("active");
     buttonPasting.classList.remove("active")
 });
+
+// Slider line //
+
+for (let i = 0; i < slides.length; i++) {
+    let slide = slides[i]
+    console.log(slide);
+}
+
+function nextButton(slide) {
+    slide++
+}
+
+// nextButton.addEventListener('click', (nextButton) => {
+    
+// });
+
+
+// prevButton.addEventListener('click', (_button) => {
+// });
