bbfa6d54bda55901bb3947f84cda14d679d17597
Toni01~ <neonver.2020@gmail.com>
2023-12-05 20:43:14 +0900

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 const changeService = document.querySelectorAll('.our-services__button');
 const buttonPasting = document.querySelector('#buttonPasting');
 const buttonDetailing = document.querySelector('#buttonDetailing');
-const slider = document.querySelector('.car__slide-line');
-const slides = Array.from(slider.querySelectorAll('.car__section'));
-const prevButton = document.querySelector('#prevCarButton');
-const nextButton = document.querySelector('#nextCarButton');
-const slideCount = slides.length;
-let slideIndex = 0;
-// OUR SERVICES button //
+const carSlider = document.querySelector('.car__slide-line');
+const carSlides = Array.from(carSlider.querySelectorAll('.car__section'));
+const advantagesSlider = document.querySelector('.our-advantages__scroll-line');
+const advantagesSlides = Array.from(advantagesSlider.querySelectorAll('.our-advantages__scroll-text'));
+const carPrev = document.querySelector('#prevCarButton');
+const carNext = document.querySelector('#nextCarButton');
+const advantagesPrev = document.querySelector('#prevAdvantagesButton');
+const advantagesNext = document.querySelector('#nextAdvantagesButton');
+const carCount = carSlides.length;
+const advantagesCount = advantagesSlides.length;
+let carIndex = 0;
+let advantagesIndex = 0;
 
+
+// OUR SERVICES button //
 // Set event handlers for buttons
 buttonPasting.addEventListener('click', changesServices);
 buttonDetailing.addEventListener('click', changesServices);
-
 // Function for updating the buttons
 function changesServices() {
     for(var i = 0; i < changeService.length; i++) {
@@ -25,34 +31,60 @@ function changesServices() {
     }
 };
 
-// Slider line //
+
+// carSlider line //
 
 // Set event handlers for buttons
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
+carPrev.addEventListener('click', carPreviousSlide);
+carNext.addEventListener('click', carNextSlide);
 // Function for showing the previous slide
-function showPreviousSlide() {
-    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-    updateSlider();
+function carPreviousSlide() {
+    carIndex = (carIndex - 1 + carCount) % carCount;
+    updateCar();
 }
-
 // Function for displaying the next slide
-function showNextSlide() {
-    slideIndex = (slideIndex + 1) % slideCount;
-    updateSlider();
+function carNextSlide() {
+    carIndex = (carIndex + 1) % carCount;
+    updateCar();
 }
-
 // Function to update the slider display
-function updateSlider() {
-slides.forEach((slide, index) => {
-    if (index === slideIndex) {
+function updateCar() {
+    carSlides.forEach((slide, index) => {
+    if (index === carIndex) {
         slide.style.display = 'flex';
     } else {
         slide.style.display = 'none';
     }
 });
 }
-
 // Initializing the slider
-updateSlider();
+updateCar();
+
+
+// advantagesSlider line //
+
+// Set event handlers for buttons
+advantagesPrev.addEventListener('click', showPreviousSlide);
+advantagesNext.addEventListener('click', showNextSlide);
+// Function for showing the previous slide
+function showPreviousSlide() {
+    advantagesIndex = (advantagesIndex - 1 + advantagesCount) % advantagesCount;
+    updateAdvantages();
+}
+// Function for displaying the next slide
+function showNextSlide() {
+    advantagesIndex = (advantagesIndex + 1) % advantagesCount;
+    updateAdvantages();
+}
+// Function to update the slider display
+function updateAdvantages() {
+    advantagesSlides.forEach((slide, index) => {
+    if (index === advantagesIndex) {
+        slide.style.display = 'flex';
+    } else {
+        slide.style.display = 'none';
+    }
+});
+}
+// Initializing the slider
+updateAdvantages();
